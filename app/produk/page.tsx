c960525cd13d7c6e1872bffa54d504a8d58f6dc5
naufalchoirulaananda <naufalchoirulananda@zohomail.com>
2024-01-23 11:55:17 +0700

    tambah link to ampyang page

--- app/produk/page.tsx
+++ app/produk/page.tsx
@@ -76,7 +76,7 @@ const Page = () => {
               />
             </a>
             <div className="p-5">
-              <a href="/subproduk/purecleans">
+              <a href="/subproduk/ampyang">
                 <h5 className="mb-2 text-2xl font-bold tracking-wider text-gray-900">
                   AMPYANG
                 </h5>
@@ -87,7 +87,7 @@ const Page = () => {
                 Usaha &quot;Kenangan&quot;.
               </p>
               <a
-                href="/subproduk/purecleans"
+                href="/subproduk/ampyang"
                 className="inline-flex items-center px-3 py-2 text-[12px] font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Baca selengkapnya
